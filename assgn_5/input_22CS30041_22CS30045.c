76f20b280e90efb1491e22b34a91cb4746e5bd6c
Parth Patil <psp90804@gmail.com>
2024-09-26 02:03:21 +0530

    Added parse tree

---
+++ assgn_5/input_22CS30041_22CS30045.c
@@ -0,0 +1,5 @@
+int main()
+{
+    int a = 10;
+    float d = 2.5;
+}
---
+++ assgn_5/tinyC_22CS30041_22CS30045.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "y.tab.h"
+
+void yyerror(char *s);
+extern int yyparse();
+
+struct _linkednodelist;
+
+struct _node
+{
+    struct _linkednodelist *children;
+    char *data;
+};
+
+struct _linkednodelist
+{
+    struct _node *node;
+    struct _linkednodelist *next;
+};
+
+typedef struct _node *node;
+typedef struct _linkednodelist *linkednodelist;
+
+node newnode(char *data)
+{
+    node n = (node)malloc(sizeof(struct _node));
+    n->data = strdup(data);
+    n->children = NULL;
+    return n;
+}
+
+linkednodelist newlinkednode(node n)
+{
+    linkednodelist l = (linkednodelist)malloc(sizeof(struct _linkednodelist));
+    l->node = n;
+    l->next = NULL;
+    return l;
+}
+
+void addchild(node parent, node child)
+{
+    if (parent->children == NULL)
+    {
+        parent->children = newlinkednode(child);
+        return;
+    }
+
+    linkednodelist temp = parent->children;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = newlinkednode(child);
+}
+
+void printtree(node n, int depth)
+{
+    for (int i = 0; i < depth; i++)
+    {
+        printf("  ");
+    }
+    printf("%s\n", n->data);
+    linkednodelist temp = n->children;
+    while (temp != NULL)
+    {
+        printtree(temp->node, depth + 1);
+        temp = temp->next;
+    }
+}
+
+void yyerror(char *s)
+{
+    fprintf(stderr, "%s\n", s);
+}
+
+int main()
+{
+    yyparse();
+    return 0;
+}
